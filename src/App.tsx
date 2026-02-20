f3ac29f2bdcd83efc9a3539bcabbf5c139e9f229
mycampushub <mycampuslifestyle@gmail.com>
2026-02-20 15:45:13 +0600

    few fixes in index and app file

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import SalesRoutes from './pages/SalesRoutes';
 import HumanResourcesRoutes from './pages/HumanResourcesRoutes';
 import MasterDataRoutes from './pages/MasterDataRoutes';
 import BusinessIntelligenceRoutes from './pages/BusinessIntelligenceRoutes';
-import TrialCenter from './pages/TrialCenter';
+rialCenter from './pages/TrialCenter';
 import NotFound from './pages/NotFound';
 
 function App() {
